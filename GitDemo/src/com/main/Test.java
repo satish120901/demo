5c30a6dbed52de926a8b5fad9d9e20e840a5b29f
satish120901 <satishagrawal236@gmail.com>
2025-01-23 15:10:52 +0530

    Update Test.java

--- GitDemo/src/com/main/Test.java
+++ GitDemo/src/com/main/Test.java
@@ -6,12 +6,17 @@ public class Test {
 		System.out.println("m1---");
 	}
 
+	public void m2(){
+		System.out.println("m2--");
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		
 		Test t=new Test();
 		t.m1();
+		t.m2();
 	}
 
 }
