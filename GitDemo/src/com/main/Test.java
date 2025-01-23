bbf5a9419f4a056839da52794e3d459e3aaba7bb
satis <satis@SatishAgrawal>
2025-01-23 15:07:45 +0530

    m1 method added

---
+++ GitDemo/src/com/main/Test.java
@@ -0,0 +1,17 @@
+package com.main;
+
+public class Test {
+	
+	public void m1() {
+		System.out.println("m1---");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Test t=new Test();
+		t.m1();
+	}
+
+}
